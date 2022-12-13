c8ced06a7f3014ba19fe0f798133cacd5dda14eb
gitess2 <tesshwalker10@gmail.com>
2022-12-13 09:07:25 -0500

    sorry

--- main.go
+++ main.go
@@ -2,15 +2,45 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome!</h>")
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/index.html"))
+	tmpl.Execute(w, nil)
+}
+
+func chatHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/chat.html"))
+	tmpl.Execute(w, nil)
+}
+
+func mediaHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/media.html"))
+	tmpl.Execute(w, nil)
+}
+
+// I love Air
+
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path { // returns the URL and path
+	case "/":
+		homeHandler(w, r)
+	case "/chat":
+		chatHandler(w, r)
+	case "/media":
+		mediaHandler(w, r)
+	default:
+		// Page not found :(
+		http.NotFound(w, r)
+	}
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	var router Router
 	fmt.Println("Starting server on :8080...")
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", router)
 }
